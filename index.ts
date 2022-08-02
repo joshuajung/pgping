1ea0b4c058e9763bb5df8f7d92dc3d6a4591c1e5
Joshua Jung <mail@joshua-jung.com>
2022-08-02 07:28:48 +0200

    Ping HTTP Requests instead

--- index.ts
+++ index.ts
@@ -1,23 +1,24 @@
-import Logger from "@logdna/logger";
+import axios from "axios";
 import http from "http";
-import * as pg from "pg";
 
-const connectionString = process.env.CONNECTION_STRING;
-const logdnaKey = process.env.LOGDNA_KEY;
+const host = process.env.HOST;
+const logdnaKey = process.env.LOGDNA_KEY ?? "asd";
 const disabled = process.env.DISABLED;
-var logger = Logger.createLogger(logdnaKey!);
+// var logger = Logger.createLogger(logdnaKey!);
+const url = `https://${host}/api/dev/health`;
 
 const ping = async () => {
   try {
     console.log("⚪️  PING");
     const start = Date.now();
-    const connection = new pg.Client({ connectionString });
-    await connection.connect();
-    await connection.end();
+    await axios.get(url);
     const delta = Date.now() - start;
-    console.log("🟢  PONG after", delta);
-    logger.log(`[pgpong] delta=${delta}`);
-  } catch {}
+    const indicator = delta < 1000 ? "🟢" : "🔴";
+    console.log(indicator, "  PONG after", delta);
+    // logger.log(`[pgpong] delta=${delta}`);
+  } catch (e) {
+    console.error(e);
+  }
   setTimeout(async () => {
     ping();
   }, 1000);
@@ -26,7 +27,7 @@ if (disabled) {
   console.debug("Disabled");
 } else {
   console.debug("Starting…");
-  logger.log(`[pgpong] start`);
+  // logger.log(`[pgpong] start`);
   ping();
 }
 http.createServer().listen(parseInt(process.env.PORT ?? "11999"), "0.0.0.0");
